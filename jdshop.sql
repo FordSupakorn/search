9662a32abad389ac16219a078367e31b585ef984
AomPanuwit <tomtomza45@outlook.com>
2019-12-28 08:47:27 +0700

    28/12/2562

---
+++ jdshop.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 20, 2019 at 09:24 PM
+-- Server version: 8.0.17
+-- PHP Version: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jdshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `price` double NOT NULL,
+  `unitInstock` int(11) NOT NULL,
+  `picture` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `description`, `price`, `unitInstock`, `picture`) VALUES
+(1, 'Lenovo IdeaPad', 'Lenovo IdeaPad', 20000, 20, '010.jpg'),
+(2, 'Asus Zenbook', 'Asus Zenbook', 20000, 245, '011.jpg'),
+(3, 'Dell Inspiron', 'Dell Inspiron', 15000, 555, '012.jpg'),
+(4, 'Notebook MSI', 'Notebook MSI', 15000, 4634, '012.jpg'),
+(5, 'Notebook Lenovo', 'Notebook Lenovo', 17000, 21, '013.jpg'),
+(6, 'Notebook Lenovo IdeaPad', 'Notebook Lenovo IdeaPad', 17000, 8546, '018.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- updataproduct.php
+++ updataproduct.php
@@ -6,7 +6,13 @@
     $description = $_POST['txtDescription'];
     $price = $_POST['txtPrice'];
     $unitInstock = $_POST['txtStock'];
+    //updata picture
     $picture= $_POST['hdnProductPic'];
+    if($_FILES["filePic"]["name"]!=""){
+        $picture = $_FILES["filePic"]["name"];
+
+        move_uploaded_file($_FILES["filePic"]["tmp_name"],"pig/".$_FILES["filePic"]["name"]);
+    }
 
     $sql = "UPDATE product SET name='$name', description='$description', price=$price,
     unitInStock=$unitInstock, picture='$picture' WHERE id = $pid";
